00ca7f4e4a736d451d33ed92388a6cc4eb0c63af
Tommy van der Vorst <tommy@pixelspark.nl>
2018-08-11 11:37:03 +0200

    Show current light color

--- src/components/HueLight.vue
+++ src/components/HueLight.vue
@@ -5,7 +5,7 @@
 			{{Math.round(light.state.bri/255*100)}} % <br/>
 		</template>
 		<button v-on:click="toggle(!light.state.on)">Toggle</button>
-		<input type="color" v-on:change="colour" v-if="light.state.hue">
+		<input type="color" v-on:change="colour" v-if="light.state.hue" :value="hexColor">
 	</div>
 </template>
 
@@ -41,6 +41,40 @@ export default {
 
 			const colour = [x, y];
 			this.$emit('call', "/lights/" + this.hueId + "/state", {on: true, xy: colour});
+		}
+	},
+
+	computed: {
+		hexColor() {
+			let state = this.light.state;
+
+			const x = state.xy[0]; // the given x value
+			const y = state.xy[1]; // the given y value
+			const z = 1.0 - x - y;
+			const Y = state.bri; // The given brightness value
+			const X = (Y / y) * x;
+			const Z = (Y / y) * z;
+
+			const red1 = X * 1.656492 - Y * 0.354851 - Z * 0.255038;
+			const green1 = -X * 0.707196 + Y * 1.655397 + Z * 0.036152;
+			const blue1 = X * 0.051713 - Y * 0.121364 + Z * 1.011530;
+
+			const red2 = (red1 <= 0.0031308 ? 12.92 * red1 : (1.0 + 0.055) * Math.pow(red1, (1.0 / 2.4)) - 0.055);
+			const green2 = (green1 <= 0.0031308 ? 12.92 * green1 : (1.0 + 0.055) * Math.pow(green1, (1.0 / 2.4)) - 0.055);
+			const blue2 = (blue1 <= 0.0031308 ? 12.92 * blue1 : (1.0 + 0.055) * Math.pow(blue1, (1.0 / 2.4)) - 0.055);
+
+			const correction = Math.max(red2, green2, blue2);
+
+			const red = Math.floor(Math.max(red2 / correction, 0) * 255);
+			const green = Math.floor(Math.max(green2 / correction, 0) * 255);
+			const blue = Math.floor(Math.max(blue2 / correction, 0) * 255);
+
+			const colourHex = "#" +
+				red.toString(16).padStart(2, "0") +
+				green.toString(16).padStart(2, "0") +
+				blue.toString(16).padStart(2, "0");
+
+			return colourHex;
 		}
 	}
 };
